a462dc171d4325419dc0bb1423b8a36306f945ed
marisofip <marisofipalencia@gmail.com>
2023-01-09 23:07:43 +0000

    cargando tablas

--- src/models.py
+++ src/models.py
@@ -8,23 +8,59 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+	
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    url = Column(String(250), nullable=False)
+    description = Column(String(800), nullable=False)
+    user = relationship('User', back_populates='post')
+   
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'),nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    description = Column(String(800), nullable=False)
+    user = relationship('User', back_populates='comment')
+    post = relationship('Post', back_populates='comment')
+	
+class Like(Base):
+    __tablename__ = 'like'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'),nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'),nullable=False)
+    description = Column(String(800), nullable=False)
+    user = relationship('User', back_populates='like')
+    post = relationship('Post', back_populates='like')
+
+
+class Followers(Base):
+    __tablename__ = 'followers'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'),nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'),nullable=False)
+    user_to = relationship('User', back_populates='followers')
+    user_from = relationship('User', back_populates='followers')
 
     def to_dict(self):
         return {}
